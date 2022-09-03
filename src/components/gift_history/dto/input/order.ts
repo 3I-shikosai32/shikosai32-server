72ad888e19fe802b52ed47ca4e1d584893e89772
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-04 03:41:37 +0900

    feat: ✨ GiftHistoryOrderInput GraphQL Inputを作成

---
+++ src/components/gift_history/dto/input/order.ts
@@ -0,0 +1,8 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { SortOrder } from '@/libs/prisma/generated/prisma/sort-order/enum';
+
+@InputType()
+export default class GiftHistoryOrderInput {
+  @Field(() => SortOrder, { nullable: true })
+  createdAt?: keyof typeof SortOrder;
+}
