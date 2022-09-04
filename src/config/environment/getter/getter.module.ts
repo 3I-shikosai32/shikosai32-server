42ed989c36036045b185dd3bf2b1b6a6bd527e04
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-05 04:23:50 +0900

    feat: ✨ (EnvModule) NODE_ENVの値に応じてenvFilePathが変わるように修正

--- src/config/environment/getter/getter.module.ts
+++ src/config/environment/getter/getter.module.ts
@@ -1,13 +1,20 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { match } from 'ts-pattern';
 import validate from '../validator';
 import Env from './getter.service';
+
+const envFilePath = match(process.env.NODE_ENV)
+  .with('development', () => ['.env', '.env.development'])
+  .with('production', () => ['.env', '.env.production'])
+  .with('test', () => ['.env', '.env.test'])
+  .otherwise(() => ['.env', '.env.development']);
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env'],
+      envFilePath,
       validate,
       isGlobal: true,
     }),
