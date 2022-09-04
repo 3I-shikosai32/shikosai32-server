f9d2fb0555ee70629334f56e5f0966d90d270e99
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-04 16:56:35 +0900

    feat: ✨ itemServiceの実装
    ・findUnique
    ・findAll
    ・create
    ・update
    ・delete
    の実装

---
+++ src/components/item/item.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import PrismaService from '@/libs/prisma/prisma.service';
+
+@Injectable()
+export default class ItemService {
+  constructor(private prismaService: PrismaService) {}
+
+  async findUnique(args: { where: Prisma.ItemWhereUniqueInput }) {
+    return this.prismaService.item.findUnique({
+      ...args,
+    });
+  }
+
+  async findAll() {
+    return this.prismaService.item.findMany({});
+  }
+
+  async create(args: { data: Prisma.ItemCreateInput }) {
+    return this.prismaService.item.create({
+      ...args,
+    });
+  }
+
+  async update(args: { where: Prisma.ItemWhereUniqueInput; data: Prisma.ItemUpdateInput }) {
+    return this.prismaService.item.update({
+      ...args,
+    })
+  }
+
+  async delete(args: { where: Prisma.ItemWhereUniqueInput }) {
+    return this.prismaService.item.delete({
+      ...args,
+    });
+  }
+}
