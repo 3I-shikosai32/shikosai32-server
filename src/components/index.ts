61a7f6be040dfeb21e15cf9d63ae8eae9e884773
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-04 18:17:30 +0900

    feat: ✨ (Components) GiftHistoryModuleを追加

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
+import GiftHistoryModule from './gift_history/gift_history.module';
 import UserModule from './user/user.module';
 
-const Components = [UserModule];
+const Components = [UserModule, GiftHistoryModule];
 
 export default Components;
