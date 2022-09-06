bc953c44715526758e1d1f1a9adc0e9079b27e3b
shio <85730998+dino3616@users.noreply.github.com>
2022-09-07 00:52:17 +0900

    refactor: ♻️ (ItemService) コードをフォーマット

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import GiftModule from './gift/gift.module';
 import GiftHistoryModule from './gift_history/gift_history.module';
-import ItemModule from './item/item.module'
+import ItemModule from './item/item.module';
 import UserModule from './user/user.module';
 
 
