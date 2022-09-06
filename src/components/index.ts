4ef443792041bb119dd6c94c32b165cfe1834c1e
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-07 00:22:08 +0900

    fix: 🐛 (components) ComponentsにItemModuleを追加

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import GiftModule from './gift/gift.module';
 import GiftHistoryModule from './gift_history/gift_history.module';
+import ItemModule from './item/item.module'
 import UserModule from './user/user.module';
 
-const Components = [UserModule, GiftModule, GiftHistoryModule];
+
+const Components = [UserModule, GiftModule, GiftHistoryModule, ItemModule];
 
 export default Components;
