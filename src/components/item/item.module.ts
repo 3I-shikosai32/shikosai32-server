f6feb454f2d6577c1cd059e1afd0e02a89317247
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-07 00:24:52 +0900

    fix: 🐛 (Item) Resolverの消去とそれに伴うItemModuleの修正

--- src/components/item/item.module.ts
+++ src/components/item/item.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import ItemMutation from './item.resolver.mutation';
-import ItemQury from './item.resolver.query';
-import ItemSubscription from './item.resolver.subscription';
 import ItemService from './item.service';
 
 @Module({
-  providers: [ItemService, ItemQury, ItemSubscription, ItemMutation],
+  providers: [ItemService],
 })
 export default class ItemModule {}
