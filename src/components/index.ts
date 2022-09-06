b24870d8456fc917c085b757c4069194eec6b4f6
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-07 01:03:51 +0900

    refactor: ♻️ コードをフォーマット

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ import GiftHistoryModule from './gift_history/gift_history.module';
 import ItemModule from './item/item.module';
 import UserModule from './user/user.module';
 
-
 const Components = [UserModule, ItemModule, GiftModule, GiftHistoryModule];
 
 export default Components;
