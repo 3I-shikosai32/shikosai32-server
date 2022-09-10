214caee7b0151b79fd8393463f04e2adf8e75265
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-10 23:25:34 +0900

    feat: ✨ (GiftHistory Mutation Resolver Test) exchangeGiftのテストを作成

---
+++ src/components/gift_history/gift_history.resolver.mutation.spec.ts
@@ -0,0 +1,95 @@
+import { Character, Game, Gift, GiftHistory, GiftName, Item, Role, User } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import GiftService from '../gift/gift.service';
+import UserService from '../user/user.service';
+import GiftHistoryMutation from './gift_history.resolver.mutation';
+import GiftHistoryService from './gift_history.service';
+
+describe('GiftHistory Mutation Resolver Test', () => {
+  let mockedUserService: DeepMockProxy<UserService>;
+  let mockedGiftService: DeepMockProxy<GiftService>;
+  let mockedGiftHistoryService: DeepMockProxy<GiftHistoryService>;
+  let giftHistoryMutation: GiftHistoryMutation;
+
+  type UserModel = User & { items: (Item & { users: User[] })[]; giftHistories: (GiftHistory & { exchangedGift: Gift })[] };
+  type GiftModel = Gift & { giftHistories: (GiftHistory & { user: User })[] };
+  type GiftHistoryModel = GiftHistory & { user: User & { giftHistories: GiftHistory[] }; exchangedGift: Gift & { giftHistories: GiftHistory[] } };
+
+  beforeEach(() => {
+    mockedUserService = mockDeep<UserService>();
+    mockedGiftService = mockDeep<GiftService>();
+    mockedGiftHistoryService = mockDeep<GiftHistoryService>();
+    giftHistoryMutation = new GiftHistoryMutation(mockedGiftHistoryService, mockedUserService, mockedGiftService);
+  });
+
+  test('exchangeGift', async () => {
+    const giftFindUniqueRes: GiftModel = {
+      id: 'abc-123',
+      name: GiftName.BABY_STAR,
+      iconUrl: 'https://example.com',
+      price: 10,
+      remaining: 1,
+      giftHistories: [],
+      createdAt: new Date(),
+    };
+    mockedGiftService.findUnique.mockResolvedValue(giftFindUniqueRes);
+
+    const userFindUniqueRes: UserModel = {
+      id: 'def-123',
+      name: 'fake user',
+      email: 'test@example.com',
+      role: Role.USER,
+      totalPointDay1: 10,
+      totalPointDay2: 0,
+      consumablePoint: 10,
+      character: Character.CAT,
+      iconUrl: 'https://example.com',
+      avatarUrl: 'https://example.com',
+      items: [],
+      itemIds: [],
+      participateGame: Game.NONE,
+      pullableGachaTimes: 0,
+      giftHistories: [],
+      createdAt: new Date(),
+    };
+    mockedUserService.findUnique.mockResolvedValue(userFindUniqueRes);
+
+    const userUpdateRes: UserModel = {
+      id: 'def-123',
+      name: 'fake user',
+      email: 'test@example.com',
+      role: Role.USER,
+      totalPointDay1: 10,
+      totalPointDay2: 0,
+      consumablePoint: 0,
+      character: Character.CAT,
+      iconUrl: 'https://example.com',
+      avatarUrl: 'https://example.com',
+      items: [],
+      itemIds: [],
+      participateGame: Game.NONE,
+      pullableGachaTimes: 0,
+      giftHistories: [],
+      createdAt: new Date(),
+    };
+    mockedUserService.update.mockResolvedValue(userUpdateRes);
+
+    const giftHistoryCreateRes: GiftHistoryModel = {
+      id: 'def-123',
+      isDelivered: false,
+      user: userUpdateRes,
+      userId: 'def-123',
+      exchangedGift: giftFindUniqueRes,
+      giftId: 'abc-123',
+      createdAt: new Date(),
+    };
+    mockedGiftHistoryService.create.mockResolvedValue(giftHistoryCreateRes);
+
+    const createdGiftHistories = await giftHistoryMutation.exchangeGift({
+      data: { isDelivered: false, userId: userFindUniqueRes.id, giftId: giftFindUniqueRes.id },
+      exchangeQuantity: 1,
+    });
+
+    await expect(createdGiftHistories).toEqual(expect.arrayContaining([giftHistoryCreateRes]));
+  });
+});
