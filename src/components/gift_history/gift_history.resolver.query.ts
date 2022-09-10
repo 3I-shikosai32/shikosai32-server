d069694632d70d9fe6750dd378126d7e95b18839
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-10 23:22:53 +0900

    feat: ⚰️ (GiftHistoryQuery) 不要になった一時Resolverを削除

--- src/components/gift_history/gift_history.resolver.query.ts
+++ src/components/gift_history/gift_history.resolver.query.ts
@@ -1,12 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
 import GiftHistoryService from './gift_history.service';
 
 @Resolver()
 export default class GiftHistoryQuery {
   constructor(private service: GiftHistoryService) {}
-
-  @Query(() => String)
-  async tmp() {
-    return 'tmp';
-  }
 }
