2bf58a2cbd45819fe86881f58635b6e17c30c1c2
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-11 01:11:30 +0900

    feat: ✨ (GiftHistoryQuery) findGiftHistory GraphQL Queryを作成

--- src/components/gift_history/gift_history.resolver.query.ts
+++ src/components/gift_history/gift_history.resolver.query.ts
@@ -1,12 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import FindGiftHistoryArgs from './dto/args/findGiftHistory';
+import GiftHistory from './dto/object';
 import GiftHistoryService from './gift_history.service';
 
 @Resolver()
 export default class GiftHistoryQuery {
   constructor(private service: GiftHistoryService) {}
 
-  @Query(() => String)
-  async tmp() {
-    return 'tmp';
+  @Query(() => GiftHistory, { nullable: true })
+  async findGiftHistory(@Args() args: FindGiftHistoryArgs): Promise<GiftHistory | null> {
+    const giftHistory = await this.service.findUnique(args);
+
+    return giftHistory;
   }
 }
