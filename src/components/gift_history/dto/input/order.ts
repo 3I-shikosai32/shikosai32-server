2739140726f9691175f8d0ea5777ce69b296a606
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-11 22:09:47 +0900

    fix: 🐞 GiftHistory.deliveredの追加によって生じた不整合を解消

--- src/components/gift_history/dto/input/order.ts
+++ src/components/gift_history/dto/input/order.ts
@@ -5,4 +5,7 @@ import { SortOrder } from '@/libs/prisma/generated/prisma/sort-order/enum';
 export default class GiftHistoryOrderInput {
   @Field(() => SortOrder, { nullable: true })
   createdAt?: keyof typeof SortOrder;
+
+  @Field(() => SortOrder, { nullable: true })
+  deliveredAt?: keyof typeof SortOrder;
 }
--- src/components/gift_history/dto/input/update.ts
+++ src/components/gift_history/dto/input/update.ts
@@ -4,4 +4,7 @@ import { InputType, Field } from '@nestjs/graphql';
 export default class GiftHistoryUpdateInput {
   @Field(() => Boolean, { nullable: false })
   isDelivered: boolean;
+
+  @Field(() => Date, { nullable: false, defaultValue: new Date() })
+  deliveredAt: Date;
 }
