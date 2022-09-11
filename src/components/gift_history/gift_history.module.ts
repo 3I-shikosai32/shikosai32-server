ba360df9193731f33c5958be2b7c6cb494fdf58f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-11 18:22:40 +0900

    fix: 🐛 (GiftHistoryModule) DIのバグを修正
    GiftHistoryの依存関係にUserServiceとGiftServiceを追加することで、DIのバグを修正した。

--- src/components/gift_history/gift_history.module.ts
+++ src/components/gift_history/gift_history.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import GiftService from '../gift/gift.service';
+import UserService from '../user/user.service';
 import GiftHistoryMutation from './gift_history.resolver.mutation';
 import GiftHistoryQuery from './gift_history.resolver.query';
 import GiftHistorySubscription from './gift_history.resolver.subscription';
 import GiftHistoryService from './gift_history.service';
 
 @Module({
-  providers: [GiftHistoryService, GiftHistoryQuery, GiftHistoryMutation, GiftHistorySubscription],
+  providers: [GiftHistoryService, GiftHistoryQuery, GiftHistoryMutation, GiftHistorySubscription, UserService, GiftService],
 })
 export default class GiftHistoryModule {}
