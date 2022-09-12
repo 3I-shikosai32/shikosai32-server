568c5e98d13a2ceaeebc2460d2b18454a9fb554f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-12 21:59:59 +0900

    fix: 🐞 (UserUpdateInput) 変更時に特別な処理を必要とするフィールドを削除

--- src/components/user/dto/input/update.ts
+++ src/components/user/dto/input/update.ts
@@ -1,27 +1,11 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
-import { Game } from '@/libs/prisma/generated/prisma/game/enum';
+import { InputType, Field } from '@nestjs/graphql';
 import { Role } from '@/libs/prisma/generated/prisma/role/enum';
 
 @InputType()
 export default class UserUpdateInput {
+  @Field(() => String, { nullable: true })
+  name?: string;
+
   @Field(() => Role, { nullable: true })
   role?: keyof typeof Role;
-
-  @Field(() => Int, { nullable: true })
-  totalPointDay1?: number;
-
-  @Field(() => Int, { nullable: true })
-  totalPointDay2?: number;
-
-  @Field(() => Int, { nullable: true })
-  consumablePoint?: number;
-
-  @Field(() => [String], { nullable: true })
-  itemIds?: string[];
-
-  @Field(() => Game, { nullable: true })
-  participateGame?: keyof typeof Game;
-
-  @Field(() => Int, { nullable: true })
-  pullableGachaTimes?: number;
 }
