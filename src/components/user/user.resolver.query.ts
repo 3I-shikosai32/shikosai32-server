07658e99ebe4b5b6af573682f984074df3fb3bc5
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-13 01:39:53 +0900

    feat: ✨ (UserQuery) getObtainmentStatuses GraphQL Queryを作成

--- src/components/user/user.resolver.query.ts
+++ src/components/user/user.resolver.query.ts
@@ -1,12 +1,15 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
+import ItemService from '../item/item.service';
 import FindUserArgs from './dto/args/findUser';
 import FindUsersArgs from './dto/args/findUsers';
+import GetObtainmentStatusesArgs from './dto/args/getObtainmentStatuses';
 import User from './dto/object';
+import ObtainmentStatus from './dto/object/obtainmentStatus';
 import UserService from './user.service';
 
 @Resolver()
 export default class UserQuery {
-  constructor(private service: UserService) {}
+  constructor(private service: UserService, private itemService: ItemService) {}
 
   @Query(() => User, { nullable: true })
   async findUser(@Args() args: FindUserArgs): Promise<User | null> {
@@ -20,5 +23,26 @@ export default class UserQuery {
     const users = await this.service.findMany(args);
 
     return users;
+  }
+
+  @Query(() => [ObtainmentStatus])
+  async getObtainmentStatuses(@Args() args: GetObtainmentStatusesArgs): Promise<ObtainmentStatus[]> {
+    const user = await this.service.findUnique(args);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const items = await this.itemService.findMany({
+      where: {
+        character: { equals: user.character },
+      },
+    });
+
+    const obtainmentStatuses = items.map((item) => ({
+      item,
+      obtained: user.itemIds.includes(item.id),
+    }));
+
+    return obtainmentStatuses;
   }
 }
