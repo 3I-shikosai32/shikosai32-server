346890d53eb35d47a089b38f8acc56cc42a7fe80
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 08:56:26 +0900

    feat: ✨ (NestedGiftInterface) NestedGiftモデルの抽象型を作成

---
+++ src/module/gift/domain/service/model/nested-gift.model.ts
@@ -0,0 +1,8 @@
+export interface NestedGiftInterface {
+  readonly id: string;
+  readonly name: string;
+  readonly iconUrl: string;
+  readonly price: number;
+  readonly remaining: number;
+  readonly createdAt: Date;
+}
