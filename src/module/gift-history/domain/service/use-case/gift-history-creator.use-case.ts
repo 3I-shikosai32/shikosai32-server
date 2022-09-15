b48c277f8040acd1c36f5e3b40fdda2f0811be2c
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 09:10:41 +0900

    feat: ✨ (GiftHistoryCreatorUseCaseInterface) GiftHistoryCreatorUseCaseの抽象を作成

---
+++ src/module/gift-history/domain/service/use-case/gift-history-creator.use-case.ts
@@ -0,0 +1,6 @@
+import { GiftHistoryInterface } from '../model/gift-history.model';
+import { ExchangeGiftArgs } from '~/gift-history/controller/dto/args/exchange-gift.args';
+
+export interface GiftHistoryCreatorUseCaseInterface {
+  exchangeGift(args: ExchangeGiftArgs): Promise<GiftHistoryInterface[]>;
+}
