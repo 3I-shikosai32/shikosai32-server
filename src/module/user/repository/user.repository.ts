2d7eaf0bb98080a4deb055b1c619f159b3a26b23
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 09:34:30 +0900

    feat: ✨ (UserRepository) UserRepositoryの具象を作成

---
+++ src/module/user/repository/user.repository.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@nestjs/common';
+import { Create, Delete, FindMany, FindUnique, Update, UserRepositoryInterface } from '../domain/service/repository/user.repository';
+import { StrictPropertyCheck } from '@/common/type/strict-property-check.type';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+
+@Injectable()
+export class UserRepository implements UserRepositoryInterface {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findUnique<T extends FindUnique>(args: StrictPropertyCheck<T, FindUnique>) {
+    const foundUser = await this.prismaService.user.findUnique({
+      ...args,
+      include: {
+        items: {
+          include: {
+            users: true,
+          },
+        },
+        giftHistories: {
+          include: {
+            exchangedGift: true,
+          },
+        },
+      },
+    });
+
+    return foundUser;
+  }
+
+  async findMany<T extends FindMany>(args?: StrictPropertyCheck<T, FindMany>) {
+    const foundUsers = await this.prismaService.user.findMany({
+      ...args,
+      include: {
+        items: {
+          include: {
+            users: true,
+          },
+        },
+        giftHistories: {
+          include: {
+            exchangedGift: true,
+          },
+        },
+      },
+    });
+
+    return foundUsers;
+  }
+
+  async create<T extends Create>(args: StrictPropertyCheck<T, Create>) {
+    const createdUser = await this.prismaService.user.create({
+      ...args,
+      include: {
+        items: {
+          include: {
+            users: true,
+          },
+        },
+        giftHistories: {
+          include: {
+            exchangedGift: true,
+          },
+        },
+      },
+    });
+
+    return createdUser;
+  }
+
+  async update<T extends Update>(args: StrictPropertyCheck<T, Update>) {
+    const updatedUser = await this.prismaService.user.update({
+      ...args,
+      include: {
+        items: {
+          include: {
+            users: true,
+          },
+        },
+        giftHistories: {
+          include: {
+            exchangedGift: true,
+          },
+        },
+      },
+    });
+
+    return updatedUser;
+  }
+
+  async delete<T extends Delete>(args: StrictPropertyCheck<T, Delete>) {
+    const deleteGiftHistories = this.prismaService.giftHistory.deleteMany({ where: { userId: args?.where.id } });
+    const deleteUser = this.prismaService.user.delete({
+      ...args,
+      include: {
+        items: {
+          include: {
+            users: true,
+          },
+        },
+        giftHistories: {
+          include: {
+            exchangedGift: true,
+          },
+        },
+      },
+    });
+
+    const [, deletedUser] = await this.prismaService.$transaction([deleteGiftHistories, deleteUser]);
+
+    return deletedUser;
+  }
+}
