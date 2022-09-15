26b8e2598d120e089de10d64f5f3b0cfc35a3efc
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 22:26:33 +0900

    test: 🧪 UserCreatorUseCaseのテストを作成

---
+++ src/module/user/use-case/user-creator.use-case.spec.ts
@@ -0,0 +1,31 @@
+import { Test } from '@nestjs/testing';
+import { MockedUserRepository } from '../repository/mocked-user.repository';
+import { UserCreatorUseCase } from './user-creator.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { MockedItemRepository } from '~/item/repository/mocked-item.repository';
+
+describe('UserCreatorUseCase', () => {
+  let mockedUserRepository: MockedUserRepository;
+  let userCreatorUseCase: UserCreatorUseCase;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        { provide: InjectionToken.USER_REPOSITORY, useClass: MockedUserRepository },
+        { provide: InjectionToken.ITEM_REPOSITORY, useClass: MockedItemRepository },
+        UserCreatorUseCase,
+      ],
+    }).compile();
+
+    mockedUserRepository = moduleRef.get(InjectionToken.USER_REPOSITORY);
+    userCreatorUseCase = moduleRef.get(UserCreatorUseCase);
+  });
+
+  test('createUser', async () => {
+    const expectUser = await mockedUserRepository.create();
+
+    const createdUser = await userCreatorUseCase.createUser({ data: expectUser });
+
+    expect(createdUser).toEqual(expectUser);
+  });
+});
