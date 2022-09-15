aa0485c58b60c5d134df4ec9af2e5a17f0161f8d
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 09:13:01 +0900

    feat: ✨ (GiftHistoryReaderUseCase) GiftHistoryReaderUseCaseの具象を作成

---
+++ src/module/gift-history/use-case/gift-history-reader.use-case.ts
@@ -0,0 +1,25 @@
+import { Inject } from '@nestjs/common';
+import { FindGiftHistoriesArgs } from '../controller/dto/args/find-gift-histories.args';
+import { FindGiftHistoryArgs } from '../controller/dto/args/find-gift-history.args';
+import { GiftHistoryRepositoryInterface } from '../domain/service/repository/gift-history.repository';
+import { GiftHistoryReaderUseCaseInterface } from '../domain/service/use-case/gift-history-reader.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+
+export class GiftHistoryReaderUseCase implements GiftHistoryReaderUseCaseInterface {
+  constructor(
+    @Inject(InjectionToken.GIFT_HISTORY_REPOSITORY)
+    private readonly giftHistoryRepository: GiftHistoryRepositoryInterface,
+  ) {}
+
+  async findGiftHistory(args: FindGiftHistoryArgs) {
+    const foundGiftHistory = await this.giftHistoryRepository.findUnique(args);
+
+    return foundGiftHistory;
+  }
+
+  async findGiftHistories(args: FindGiftHistoriesArgs) {
+    const foundGiftHistories = await this.giftHistoryRepository.findMany(args);
+
+    return foundGiftHistories;
+  }
+}
