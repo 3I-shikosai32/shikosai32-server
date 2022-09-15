9b0988ce1ae0cde1bee769ed2cf690a62a17e8b9
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 22:25:33 +0900

    test: 🧪 GiftHistoryUpdaterUseCaseのテストを作成

---
+++ src/module/gift-history/use-case/gift-history-updater.use-case.spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { MockedGiftHistoryRepository } from '../repository/mocked-gift-history.repository';
+import { GiftHistoryUpdaterUseCase } from './gift-history-updater.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+
+describe('GiftHistoryUpdaterUseCase', () => {
+  let mockedGiftHistoryRepository: MockedGiftHistoryRepository;
+  let giftHistoryUpdaterUseCase: GiftHistoryUpdaterUseCase;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [{ provide: InjectionToken.GIFT_HISTORY_REPOSITORY, useClass: MockedGiftHistoryRepository }, GiftHistoryUpdaterUseCase],
+    }).compile();
+
+    mockedGiftHistoryRepository = moduleRef.get(InjectionToken.GIFT_HISTORY_REPOSITORY);
+    giftHistoryUpdaterUseCase = moduleRef.get(GiftHistoryUpdaterUseCase);
+  });
+
+  test('updateGiftHistory', async () => {
+    const expectGiftHistory = await mockedGiftHistoryRepository.update();
+
+    const updatedGiftHistory = await giftHistoryUpdaterUseCase.updateGiftHistory({ where: { id: expectGiftHistory.id }, data: expectGiftHistory });
+
+    expect(updatedGiftHistory).toEqual(expectGiftHistory);
+  });
+});
