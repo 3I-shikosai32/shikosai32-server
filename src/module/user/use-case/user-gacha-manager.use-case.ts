dd8ecffc62b1c5b9721799c1021166b8e887edb2
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-15 09:35:04 +0900

    feat: ✨ (UserGachaManagerUseCase) UserGachaManagerUseCaseの具象を作成

---
+++ src/module/user/use-case/user-gacha-manager.use-case.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PullGachaArgs } from '../controller/dto/args/pull-gacha.args';
+import { UserRepositoryInterface } from '../domain/service/repository/user.repository';
+import { UserGachaManagerUseCaseInterface } from '../domain/service/use-case/user-gacha-manager.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { ItemInterface } from '~/item/domain/service/model/item.model';
+import { ItemRepository } from '~/item/repository/item.repository';
+
+@Injectable()
+export class UserGachaManagerUseCase implements UserGachaManagerUseCaseInterface {
+  constructor(
+    @Inject(InjectionToken.USER_REPOSITORY)
+    private readonly userRepository: UserRepositoryInterface,
+    @Inject(InjectionToken.ITEM_REPOSITORY)
+    private readonly itemRepository: ItemRepository,
+  ) {}
+
+  async pullGacha(args: PullGachaArgs, pullItem: (items: ItemInterface[]) => ItemInterface) {
+    const foundUser = await this.userRepository.findUnique(args);
+    if (!foundUser) {
+      throw new Error('User not found');
+    }
+
+    const foundItems = await this.itemRepository.findMany({
+      where: {
+        character: foundUser.character,
+      },
+    });
+
+    const pulledItem = pullItem(foundItems);
+
+    const decrementedPullableGachaTimes = foundUser.pullableGachaTimes - 1; // TODO: これはModelに移す
+    if (decrementedPullableGachaTimes < 0) {
+      throw new Error('Pullable gacha times is less than 0');
+    }
+
+    await this.userRepository.update({
+      ...args,
+      data: {
+        pullableGachaTimes: decrementedPullableGachaTimes,
+      },
+    });
+
+    return pulledItem;
+  }
+}
