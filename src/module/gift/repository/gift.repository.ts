8c1a62e3d0393d76b7af3b787a1db2aa376c0e5f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-17 23:27:13 +0900

    feat: ✨ findGiftHistoriesByGiftIdを実装

--- src/module/gift/repository/gift.repository.ts
+++ src/module/gift/repository/gift.repository.ts
@@ -3,86 +3,48 @@ import { Gift } from '../domain/model/gift.model';
 import { Create, Delete, FindMany, FindUnique, GiftRepositoryInterface, Update } from '../domain/service/repository/gift.repository';
 import { StrictPropertyCheck } from '@/common/type/strict-property-check.type';
 import { PrismaService } from '@/infra/prisma/prisma.service';
+import { GiftHistory } from '~/gift-history/domain/model/gift-history.model';
 
 @Injectable()
 export class GiftRepository implements GiftRepositoryInterface {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findUnique<T extends FindUnique>(args: StrictPropertyCheck<T, FindUnique>) {
-    const foundGift = await this.prismaService.gift.findUnique({
-      ...args,
-      include: {
-        giftHistories: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
+    const foundGift = await this.prismaService.gift.findUnique(args);
 
     return foundGift ? new Gift(foundGift) : null;
   }
 
   async findMany<T extends FindMany>(args?: StrictPropertyCheck<T, FindMany>) {
-    const foundGifts = await this.prismaService.gift.findMany({
-      ...args,
-      include: {
-        giftHistories: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
+    const foundGifts = await this.prismaService.gift.findMany(args);
 
     return foundGifts.map((foundGift) => new Gift(foundGift));
   }
 
   async create<T extends Create>(args: StrictPropertyCheck<T, Create>) {
-    const createdGift = await this.prismaService.gift.create({
-      ...args,
-      include: {
-        giftHistories: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
+    const createdGift = await this.prismaService.gift.create(args);
 
     return new Gift(createdGift);
   }
 
   async update<T extends Update>(args: StrictPropertyCheck<T, Update>) {
-    const updatedGift = await this.prismaService.gift.update({
-      ...args,
-      include: {
-        giftHistories: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
+    const updatedGift = await this.prismaService.gift.update(args);
 
     return new Gift(updatedGift);
   }
 
   async delete<T extends Delete>(args: StrictPropertyCheck<T, Delete>) {
-    const deleteGiftHistories = this.prismaService.giftHistory.deleteMany({ where: { giftId: args?.where.id } });
-    const deleteGift = this.prismaService.gift.delete({
-      ...args,
-      include: {
-        giftHistories: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
+    const deleteGiftHistories = this.prismaService.giftHistory.deleteMany({ where: { giftId: args.where.id } });
+    const deleteGift = this.prismaService.gift.delete(args);
 
     const [, deletedGift] = await this.prismaService.$transaction([deleteGiftHistories, deleteGift]);
 
     return new Gift(deletedGift);
+  }
+
+  async findGiftHistoriesByGiftId<T extends FindUnique>(args: StrictPropertyCheck<T, FindUnique>) {
+    const foundGiftHistories = await this.prismaService.gift.findUnique(args).giftHistories();
+
+    return foundGiftHistories.map((foundGiftHistory) => new GiftHistory(foundGiftHistory));
   }
 }
