d2d450df9b31e44050a216d8672bf9463d514544
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-22 11:48:34 +0900

    feat: ✨ DataLoaderServiceを作成

---
+++ src/common/dataloader/dataloader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { DataLoaderInterface } from './dataloader.interface';
+import { GiftHistoryUserDataLoader } from './loaders/gift-history-user.dataloader';
+import { UserGiftHistoriesDataLoader } from './loaders/user-gift-histories.dataloader';
+
+@Injectable()
+export class DataLoaderService {
+  constructor(
+    private readonly userGiftHistoriesDataLoader: UserGiftHistoriesDataLoader,
+    private readonly giftHistoryUserDataLoader: GiftHistoryUserDataLoader,
+  ) {}
+
+  getLoaders(): DataLoaderInterface {
+    return {
+      userGiftHistoriesDataLoader: this.userGiftHistoriesDataLoader,
+      giftHistoryUserDataLoader: this.giftHistoryUserDataLoader,
+    };
+  }
+}
