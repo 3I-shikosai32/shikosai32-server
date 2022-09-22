a17eec08aefd39a85ab9b1727a2132a1e8afe2dc
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-22 11:48:13 +0900

    feat: ✨ (Loaders) 各Loaderを一括管理する配列を作成

---
+++ src/common/dataloader/loaders/index.ts
@@ -0,0 +1,4 @@
+import { GiftHistoryUserDataLoader } from './gift-history-user.dataloader';
+import { UserGiftHistoriesDataLoader } from './user-gift-histories.dataloader';
+
+export const Loaders = [UserGiftHistoriesDataLoader, GiftHistoryUserDataLoader];
