630bf2195eb48bd695ddc6de05b86e754c2ddfea
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-24 00:39:51 +0900

    feat: ✨ dataloaderを各リレーションフィールドに適用

--- src/module/gift-history/controller/gift-history.resolver.ts
+++ src/module/gift-history/controller/gift-history.resolver.ts
@@ -1,35 +1,27 @@
-import { Inject } from '@nestjs/common';
-import { Context, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { GiftHistory as GiftHistoryModel } from '../domain/model/gift-history.model';
-import { GiftHistoryReaderUseCaseInterface } from '../domain/service/use-case/gift-history-reader.use-case';
 import { GiftHistory } from './dto/object/gift-history.object';
-import { InjectionToken } from '@/common/constant/injection-token.constant';
-import { GqlContext } from '@/config/graphql/graphql-config.module';
 import { Gift } from '~/gift/controller/dto/object/gift.object';
+import { GiftDataLoader } from '~/gift/dataloader/gift.dataloader';
 import { Gift as GiftModel } from '~/gift/domain/model/gift.model';
 import { User } from '~/user/controller/dto/object/user.object';
+import { UserDataLoader } from '~/user/dataloader/user.dataloader';
 import { User as UserModel } from '~/user/domain/model/user.model';
 
 @Resolver(() => GiftHistory)
 export class GiftHistoryResolver {
-  constructor(
-    @Inject(InjectionToken.GIFT_HISTORY_READER_USE_CASE)
-    private readonly giftHistoryReaderUseCase: GiftHistoryReaderUseCaseInterface,
-  ) {}
+  constructor(private readonly userDataLoader: UserDataLoader, private readonly giftDataLoader: GiftDataLoader) {}
 
   @ResolveField(() => User)
-  async user(@Parent() giftHistory: GiftHistoryModel, @Context() ctx: GqlContext): Promise<UserModel> {
-    const user = await ctx.loader.giftHistoryUserDataLoader.load(giftHistory.userId);
+  async user(@Parent() giftHistory: GiftHistoryModel): Promise<UserModel> {
+    const user = await this.userDataLoader.load(giftHistory.userId);
 
     return user;
   }
 
   @ResolveField(() => Gift)
   async gift(@Parent() giftHistory: GiftHistoryModel): Promise<GiftModel> {
-    const gift = await this.giftHistoryReaderUseCase.findGiftByGiftHistoryId(giftHistory.id);
-    if (!gift) {
-      throw new Error('Gift not found');
-    }
+    const gift = await this.giftDataLoader.load(giftHistory.giftId);
 
     return gift;
   }
