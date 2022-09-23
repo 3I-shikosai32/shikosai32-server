c139d713c45bf61bd5748b7cea86611e4d9f4fa6
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-24 00:55:31 +0900

    feat: ✨ (UserQuery) dataloaderにキャッシュする処理を追加

--- src/module/user/controller/user-query.resolver.ts
+++ src/module/user/controller/user-query.resolver.ts
@@ -1,12 +1,13 @@
 import { Inject, Logger } from '@nestjs/common';
-import { Args, Context, Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UserDataLoader } from '../dataloader/user.dataloader';
 import { User as UserModel } from '../domain/model/user.model';
 import { UserReaderUseCaseInterface } from '../domain/service/use-case/user-reader.use-case';
 import { FindUserArgs } from './dto/args/find-user.args';
 import { FindUsersArgs } from './dto/args/find-users.args';
 import { User } from './dto/object/user.object';
+import { DataLoaderCacheService } from '@/cache/dataloader-cache.service';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
-import { GqlContext } from '@/config/graphql/graphql-config.module';
 
 @Resolver()
 export class UserQuery {
@@ -15,6 +16,8 @@ export class UserQuery {
   constructor(
     @Inject(InjectionToken.USER_READER_USE_CASE)
     private readonly readerUseCase: UserReaderUseCaseInterface,
+    private readonly userDataLoader: UserDataLoader,
+    private readonly userDataLoaderCacheService: DataLoaderCacheService<UserModel, string>,
   ) {}
 
   @Query(() => User, { nullable: true })
@@ -22,24 +25,26 @@ export class UserQuery {
     this.logger.log('findUser called');
     this.logger.log(args);
 
-    const user = await this.readerUseCase.findUser(args);
+    const foundUser = await this.readerUseCase.findUser(args);
 
-    this.logger.log(user);
+    if (foundUser) {
+      this.userDataLoaderCacheService.prime(this.userDataLoader, foundUser);
+    }
 
-    return user;
+    this.logger.log(foundUser);
+
+    return foundUser;
   }
 
   @Query(() => [User])
-  async findUsers(@Args() args: FindUsersArgs, @Context() ctx: GqlContext): Promise<UserModel[]> {
+  async findUsers(@Args() args: FindUsersArgs): Promise<UserModel[]> {
     this.logger.log('findUsers called');
     this.logger.log(args);
 
-    const users = await this.readerUseCase.findUsers(args);
+    const foundUsers = await this.readerUseCase.findUsers(args);
 
-    users.forEach((user) => {
-      ctx.loader.giftHistoryUserDataLoader.prime(user.id, user);
-    });
+    this.userDataLoaderCacheService.primeMany(this.userDataLoader, foundUsers);
 
-    return users;
+    return foundUsers;
   }
 }
