d232071cf14f9b2a4175ec6edf49bba9c02a40d1
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-23 16:43:05 +0900

    refactor: ♻️ (GiftHistoryUserDataLoader) UserDataLoaderに改名

--- src/common/dataloader/loaders/user.dataloader.ts
+++ src/common/dataloader/loaders/user.dataloader.ts
@@ -4,7 +4,7 @@ import { InjectionToken } from '@/common/constant/injection-token.constant';
 import { User } from '~/user/domain/model/user.model';
 import { UserRepositoryInterface } from '~/user/domain/service/repository/user.repository';
 
-export class GiftHistoryUserDataLoader extends BaseDataLoader<string, User> {
+export class UserDataLoader extends BaseDataLoader<string, User> {
   constructor(
     @Inject(InjectionToken.USER_REPOSITORY)
     private readonly userRepository: UserRepositoryInterface,
@@ -12,13 +12,19 @@ export class GiftHistoryUserDataLoader extends BaseDataLoader<string, User> {
     super();
   }
 
-  protected async batchLoad(keys: string[]): Promise<(User | Error)[]> {
+  protected async batchLoad(userIds: string[]): Promise<(User | Error)[]> {
     const users = await this.userRepository.findMany({
       where: {
-        id: { in: keys },
+        id: { in: userIds },
       },
     });
 
-    return users;
+    const mappedUsers = userIds.map((userId) => {
+      const user = users.find((u) => u.id === userId);
+
+      return user || new Error(`User with id ${userId} not found`);
+    });
+
+    return mappedUsers;
   }
 }
