640a72c99d1f6f48e684f6e63440ac72a3799b2f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-24 00:40:47 +0900

    feat: ✨ GiftDataLoaderを作成

---
+++ src/module/gift/dataloader/gift.dataloader.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable, Scope } from '@nestjs/common';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { BaseDataLoader } from '@/common/dataloader/base.dataloader';
+import { Gift } from '~/gift/domain/model/gift.model';
+import { GiftRepositoryInterface } from '~/gift/domain/service/repository/gift.repository';
+
+@Injectable({ scope: Scope.REQUEST })
+export class GiftDataLoader extends BaseDataLoader<string, Gift> {
+  constructor(
+    @Inject(InjectionToken.GIFT_REPOSITORY)
+    private readonly giftRepository: GiftRepositoryInterface,
+  ) {
+    super();
+  }
+
+  protected async batchLoad(giftIds: string[]): Promise<(Gift | Error)[]> {
+    const gifts = await this.giftRepository.findMany({
+      where: {
+        id: { in: giftIds },
+      },
+    });
+
+    const mappedGifts = giftIds.map((giftId) => {
+      const gift = gifts.find((g) => g.id === giftId);
+
+      return gift || new Error(`Gift with id ${giftId} not found`);
+    });
+
+    return mappedGifts;
+  }
+}
