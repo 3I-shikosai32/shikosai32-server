5279b0addef74c9a76623de23dcec4d21149c10f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-24 03:16:36 +0900

    fix: 🐞 (UserCreateInput) 不要なフィールドを削除

--- src/module/user/controller/dto/input/user-create.input.ts
+++ src/module/user/controller/dto/input/user-create.input.ts
@@ -1,8 +1,6 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { Character } from '@/infra/prisma/generated/prisma/character/enum';
-import { Game } from '@/infra/prisma/generated/prisma/game/enum';
 import { Role } from '@/infra/prisma/generated/prisma/role/enum';
-import { GiftHistoryCreateInput } from '~/gift-history/controller/dto/input/gift-history-create.input';
 
 @InputType()
 export class UserCreateInput {
@@ -18,15 +16,6 @@ export class UserCreateInput {
   @Field(() => Role, { nullable: true, defaultValue: Role.USER })
   role: keyof typeof Role;
 
-  @Field(() => Int, { nullable: true, defaultValue: 0 })
-  totalPointDay1: number;
-
-  @Field(() => Int, { nullable: true, defaultValue: 0 })
-  totalPointDay2: number;
-
-  @Field(() => Int, { nullable: true, defaultValue: 0 })
-  consumablePoint: number;
-
   @Field(() => Character, { nullable: false })
   character: keyof typeof Character;
 
@@ -35,13 +24,4 @@ export class UserCreateInput {
 
   @Field(() => String, { nullable: false })
   avatarUrl: string;
-
-  @Field(() => Game, { nullable: true, defaultValue: Game.NONE })
-  participateGame: keyof typeof Game;
-
-  @Field(() => Int, { nullable: true, defaultValue: 0 })
-  pullableGachaTimes: number;
-
-  @Field(() => [GiftHistoryCreateInput], { nullable: true })
-  giftHistories?: GiftHistoryCreateInput[];
 }
