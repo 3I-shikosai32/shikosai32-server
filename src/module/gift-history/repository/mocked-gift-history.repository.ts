00a06747068df4113799a839c8b515b61fcad1d7
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-24 21:52:53 +0900

    feat: ⚰️ (GiftHistoryRepository) 使用しないメソッドを削除
    - findUserByGiftHistoryId
    - findGiftByGiftHistoryId

--- src/module/gift-history/repository/mocked-gift-history.repository.ts
+++ src/module/gift-history/repository/mocked-gift-history.repository.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Character, Game, Role } from '@prisma/client';
 import { GiftHistory } from '../domain/model/gift-history.model';
 import { GiftHistoryRepositoryInterface } from '../domain/service/repository/gift-history.repository';
-import { Gift } from '~/gift/domain/model/gift.model';
-import { User } from '~/user/domain/model/user.model';
 
 @Injectable()
 export class MockedGiftHistoryRepository implements GiftHistoryRepositoryInterface {
@@ -80,39 +77,5 @@ export class MockedGiftHistoryRepository implements GiftHistoryRepositoryInterfa
     };
 
     return new GiftHistory(deletedGiftHistory);
-  }
-
-  async findUserByGiftHistoryId() {
-    const foundUser = {
-      id: 'abc-123',
-      name: 'fake user',
-      email: 'test@example.com',
-      role: Role.USER,
-      totalPointDay1: 0,
-      totalPointDay2: 0,
-      consumablePoint: 10,
-      character: Character.CAT,
-      iconUrl: 'https://example.com',
-      avatarUrl: 'https://example.com',
-      itemIds: [],
-      participateGame: Game.NONE,
-      pullableGachaTimes: 5,
-      createdAt: new Date('2021-01-01T00:00:00.000Z'),
-    };
-
-    return new User(foundUser);
-  }
-
-  async findGiftByGiftHistoryId() {
-    const foundGift = {
-      id: 'abc-123',
-      name: 'うまい棒',
-      iconUrl: 'https://example.com',
-      price: 10,
-      remaining: 1,
-      createdAt: new Date('2021-01-01T00:00:00.000Z'),
-    };
-
-    return new Gift(foundGift);
   }
 }
