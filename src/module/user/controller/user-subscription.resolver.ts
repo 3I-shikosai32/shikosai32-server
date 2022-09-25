763a44163d0670cf89babceeacc29236169a3374
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-25 20:36:01 +0900

    feat: ✨ (UserSubscription) updatedGameAttendersを作成

--- src/module/user/controller/user-subscription.resolver.ts
+++ src/module/user/controller/user-subscription.resolver.ts
@@ -1,7 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Subscription } from '@nestjs/graphql';
+import { GameAttenders } from '../domain/model/game-attenders.model';
 import { PubSubService } from '@/infra/pubsub/pubsub.service';
 
 @Resolver()
 export class UserSubscription {
   constructor(private readonly pubSubService: PubSubService) {}
+
+  @Subscription(() => GameAttenders)
+  async updatedGameAttenders() {
+    return this.pubSubService.asyncIterator('updatedGameAttenders');
+  }
 }
