d660d7982cdeddba3ad9524b892c1f6f2f3befa2
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-25 20:35:29 +0900

    feat: ✨ (user/use-case) UserPublisherUseCaseを作成

---
+++ src/module/user/use-case/user-publisher.ues-case.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Game } from '@prisma/client';
+import { GameAttenders } from '../domain/model/game-attenders.model';
+import { UserRepositoryInterface } from '../domain/service/repository/user.repository';
+import { UserPublisherUseCaseInterface } from '../domain/service/use-case/user-publisher.ues-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { PubSubService } from '@/infra/pubsub/pubsub.service';
+
+@Injectable()
+export class UserPublisherUseCase implements UserPublisherUseCaseInterface {
+  constructor(
+    @Inject(InjectionToken.USER_REPOSITORY)
+    private readonly userRepository: UserRepositoryInterface,
+    private readonly pubSubRepository: PubSubService,
+  ) {}
+
+  async publishUpdatedGameAttenders() {
+    const gameAttenders = await this.userRepository.findMany({
+      where: {
+        participateGame: { not: Game.NONE },
+      },
+    });
+
+    const updatedGameAttenders = {
+      xeno: gameAttenders.filter((user) => user.participateGame === Game.XENO),
+      coin_dropping: gameAttenders.filter((user) => user.participateGame === Game.COIN_DROPPING),
+      ice_raze: gameAttenders.filter((user) => user.participateGame === Game.ICE_RAZE),
+      president: gameAttenders.filter((user) => user.participateGame === Game.PRESIDENT),
+      poker: gameAttenders.filter((user) => user.participateGame === Game.POKER),
+      we_didnt_playtest: gameAttenders.filter((user) => user.participateGame === Game.WE_DIDNT_PLAYTEST),
+    };
+
+    await this.pubSubRepository.publish('updatedGameAttenders', { updatedGameAttenders });
+
+    return new GameAttenders(updatedGameAttenders);
+  }
+}
