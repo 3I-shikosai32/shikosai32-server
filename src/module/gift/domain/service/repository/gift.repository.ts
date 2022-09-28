88a2ff82d106b73807b7931c058d85606a55c14b
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-28 13:45:54 +0900

    fix: 🐞 importパスを修正

--- src/module/gift/domain/service/repository/gift.repository.ts
+++ src/module/gift/domain/service/repository/gift.repository.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client';
 import { Gift } from '../../model/gift.model';
-import { BaseRepositoryInterface } from '@/common/repository/base.repository';
+import { BaseRepositoryInterface } from '@/common/base/repository/base.repository';
 
 export type FindUnique = {
   where: Prisma.GiftWhereUniqueInput;
