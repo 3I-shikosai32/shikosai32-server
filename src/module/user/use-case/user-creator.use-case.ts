38a0d1c6f37a537b86f4e67f85ccd99c2c5dd452
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-28 19:32:23 +0900

    feat: ✨ (UserCreatorUseCase.createUser) キャラクターごとに自動でUrlを割り振るように修正

--- src/module/user/use-case/user-creator.use-case.ts
+++ src/module/user/use-case/user-creator.use-case.ts
@@ -3,6 +3,7 @@ import { CreateUserArgs } from '../controller/dto/args/create-user.args';
 import { UserRepositoryInterface } from '../domain/service/repository/user.repository';
 import { UserCreatorUseCaseInterface } from '../domain/service/use-case/user-creator.use-case';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { EnvService } from '@/config/env/env.service';
 import { ItemRepositoryInterface } from '~/item/domain/service/repository/item.repository';
 
 @Injectable()
@@ -12,6 +13,7 @@ export class UserCreatorUseCase implements UserCreatorUseCaseInterface {
     private readonly userRepository: UserRepositoryInterface,
     @Inject(InjectionToken.ITEM_REPOSITORY)
     private readonly itemRepository: ItemRepositoryInterface,
+    private readonly envService: EnvService,
   ) {}
 
   async createUser(args: CreateUserArgs) {
@@ -21,9 +23,14 @@ export class UserCreatorUseCase implements UserCreatorUseCaseInterface {
       },
     });
 
+    const iconPath = `sys/character/${args.data.character.toLowerCase()}/icon.svg?alt=media`;
+    const avatarPath = `sys/character/${args.data.character.toLowerCase()}/base.svg?alt=media`;
+
     const createdUser = await this.userRepository.create({
       data: {
         ...args.data,
+        iconUrl: `${this.envService.FirebaseStorageUrl}${encodeURIComponent(iconPath)}`,
+        avatarUrl: `${this.envService.FirebaseStorageUrl}${encodeURIComponent(avatarPath)}`,
         itemIds: foundItems.map((item) => item.id),
         items: {
           connect: foundItems.map((item) => ({ id: item.id })),
