2706af2bf4ab76ef7071a8f533cc4208d6d56550
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-28 20:56:24 +0900

    test: 🧪 (UserCreatorUseCase) TestModuleのimportsにEnvModuleを追加

--- src/module/user/use-case/user-creator.use-case.spec.ts
+++ src/module/user/use-case/user-creator.use-case.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { MockedUserRepository } from '../repository/mocked-user.repository';
 import { UserCreatorUseCase } from './user-creator.use-case';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { EnvModule } from '@/config/env/env.module';
 import { MockedItemRepository } from '~/item/repository/mocked-item.repository';
 
 describe('UserCreatorUseCase', () => {
@@ -10,6 +11,7 @@ describe('UserCreatorUseCase', () => {
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
+      imports: [EnvModule],
       providers: [
         { provide: InjectionToken.USER_REPOSITORY, useClass: MockedUserRepository },
         { provide: InjectionToken.ITEM_REPOSITORY, useClass: MockedItemRepository },
