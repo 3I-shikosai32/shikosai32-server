99fec368db5f2e69cbb6eda73b78d76aff2739e4
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-04 05:32:35 +0900

    feat: ✨ (UserResolver) characterStatusのField Resolverを追加

--- src/module/user/controller/user.resolver.ts
+++ src/module/user/controller/user.resolver.ts
@@ -1,33 +1,26 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-// import { match, P } from 'ts-pattern';
+import { UserCharacterStatusDataLoader } from '../dataloader/user-character-status.dataloader';
 import { UserGiftHistoriesDataLoader } from '../dataloader/user-gift-histories.dataloader';
 import { User as UserModel } from '../domain/model/user.model';
 import { User } from './dto/object/user.object';
+import { CharacterStatus } from '~/character-status/controller/dto/object/character-status.object';
+import { CharacterStatus as CharacterStatusModel } from '~/character-status/domain/model/character-status.model';
 import { GiftHistory } from '~/gift-history/controller/dto/object/gift-history.object';
 import { GiftHistory as GiftHistoryModel } from '~/gift-history/domain/model/gift-history.model';
-// import { Item } from '~/item/controller/dto/object/item.object';
-import { ItemDataLoader } from '~/item/dataloader/item.dataloader';
-// import { Item as ItemModel } from '~/item/domain/model/item.model';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userGiftHistoriesDataLoader: UserGiftHistoriesDataLoader, private readonly itemDataLoader: ItemDataLoader) {}
+  constructor(
+    private readonly userCharacterStatusDataLoader: UserCharacterStatusDataLoader,
+    private readonly userGiftHistoriesDataLoader: UserGiftHistoriesDataLoader,
+  ) {}
 
-  // @ResolveField(() => [Item])
-  // async items(@Parent() user: UserModel): Promise<ItemModel[]> {
-  //   const itemsOrErrors = await this.itemDataLoader.loadMany(user.itemIds);
+  @ResolveField(() => CharacterStatus)
+  async characterStatus(@Parent() user: UserModel): Promise<CharacterStatusModel> {
+    const characterStatus = await this.userCharacterStatusDataLoader.load(user.id);
 
-  //   const items = match(itemsOrErrors)
-  //     .with(P.array(P.instanceOf(Error)), (errors) => {
-  //       throw errors;
-  //     })
-  //     .with(P.array(P.instanceOf(ItemModel)), (matchedItems) => matchedItems)
-  //     .otherwise(() => {
-  //       throw new Error('Unexpected error');
-  //     });
-
-  //   return items;
-  // }
+    return characterStatus;
+  }
 
   @ResolveField(() => [GiftHistory])
   async giftHistories(@Parent() user: UserModel): Promise<GiftHistoryModel[]> {
