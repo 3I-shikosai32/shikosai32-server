ae0b026407631df8a12bd2dc760e5f27aa7703a2
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-06 13:55:10 +0900

    test: 🧪 (item-completed-history/use-case) ItemCompletedHistoryReaderUseCaseのテストを作成

---
+++ src/module/item-completed-history/use-case/item-completed-history-reader.use-case.spec.ts
@@ -0,0 +1,31 @@
+import { Test } from '@nestjs/testing';
+import { MockedItemCompletedHistoryRepository } from '../repository/mocked-item-completed-history.repository';
+import { ItemCompletedHistoryReaderUseCase } from './item-completed-history-reader.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+
+describe('ItemCompletedHistoryReaderUseCase', () => {
+  let mockedItemCompletedHistoryRepository: MockedItemCompletedHistoryRepository;
+  let giftHistoryReaderUseCase: ItemCompletedHistoryReaderUseCase;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        { provide: InjectionToken.GIFT_HISTORY_REPOSITORY, useClass: MockedItemCompletedHistoryRepository },
+        ItemCompletedHistoryReaderUseCase,
+      ],
+    }).compile();
+
+    mockedItemCompletedHistoryRepository = moduleRef.get(InjectionToken.GIFT_HISTORY_REPOSITORY);
+    giftHistoryReaderUseCase = moduleRef.get(ItemCompletedHistoryReaderUseCase);
+  });
+
+  test('findItemCompletedHistories', async () => {
+    const expectItemCompletedHistories = await mockedItemCompletedHistoryRepository.findMany();
+
+    const foundItemCompletedHistories = await giftHistoryReaderUseCase.findItemCompletedHistories({
+      id: { equals: expectItemCompletedHistories[0].id },
+    });
+
+    expect(foundItemCompletedHistories).toEqual(expectItemCompletedHistories);
+  });
+});
