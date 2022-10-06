e9c07772afc80d07dd9bf1d2e560ef680ed0ee67
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-06 10:45:24 +0900

    feat: ✨ (character-status/dto) CharacterStatusRelationFilter GraphQL Inputを作成

---
+++ src/module/character-status/controller/dto/input/character-status-relation-filter.input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { CharacterStatusWhereInput } from './character-status-where.input';
+
+@InputType()
+export class CharacterStatusRelationFilter {
+  @Field(() => CharacterStatusWhereInput, { nullable: true })
+  is?: CharacterStatusWhereInput;
+
+  @Field(() => CharacterStatusWhereInput, { nullable: true })
+  isNot?: CharacterStatusWhereInput;
+}
