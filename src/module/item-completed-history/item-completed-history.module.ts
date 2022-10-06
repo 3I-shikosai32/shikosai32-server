2257fc1b60aafcb9c01305e8e511d28426a3a7c3
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-06 13:50:42 +0900

    feat: ✨ (ItemCompletedHistoryModule) providersとexportsを追加

--- src/module/item-completed-history/item-completed-history.module.ts
+++ src/module/item-completed-history/item-completed-history.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ItemCompletedHistoryQuery } from './controller/item-completed-history-query.resolver';
+import { ItemCompletedHistoryRepository } from './repository/item-completed-history.repository';
+import { ItemCompletedHistoryReaderUseCase } from './use-case/item-completed-history-reader.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
 
-@Module({})
+@Module({
+  providers: [
+    { provide: InjectionToken.ITEM_COMPLETED_HISTORY_REPOSITORY, useClass: ItemCompletedHistoryRepository },
+    { provide: InjectionToken.ITEM_COMPLETED_HISTORY_READER_USE_CASE, useClass: ItemCompletedHistoryReaderUseCase },
+    ItemCompletedHistoryQuery,
+  ],
+  exports: [{ provide: InjectionToken.ITEM_COMPLETED_HISTORY_REPOSITORY, useClass: ItemCompletedHistoryRepository }],
+})
 export class ItemCompletedHistoryModule {}
