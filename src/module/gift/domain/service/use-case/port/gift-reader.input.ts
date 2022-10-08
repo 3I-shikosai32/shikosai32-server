b939784926eb6142aa3870b9ec1406ae0fa7937d
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-08 20:58:36 +0900

    feat: ⚰️ (gift/use-case) GiftWhere.createdAt及びGiftOrderBy.createdAtを削除

--- src/module/gift/domain/service/use-case/port/gift-reader.input.ts
+++ src/module/gift/domain/service/use-case/port/gift-reader.input.ts
@@ -1,5 +1,4 @@
 import { GiftHistoryListRelationFilter } from '@/infra/prisma/generated/gift-history/gift-history-list-relation-filter/input';
-import { DateTimeFilter } from '@/infra/prisma/generated/prisma/date-time-filter/input';
 import { IntFilter } from '@/infra/prisma/generated/prisma/int-filter/input';
 import { SortOrder } from '@/infra/prisma/generated/prisma/sort-order/enum';
 import { StringFilter } from '@/infra/prisma/generated/prisma/string-filter/input';
@@ -14,14 +13,12 @@ export type GiftWhere = {
   price?: IntFilter;
   remaining?: IntFilter;
   giftHistories?: GiftHistoryListRelationFilter;
-  createdAt?: DateTimeFilter;
 };
 
 export type GiftOrderBy = {
   name?: keyof typeof SortOrder;
   price?: keyof typeof SortOrder;
   remaining?: keyof typeof SortOrder;
-  createdAt?: keyof typeof SortOrder;
 };
 
 export type GiftCursor = {
