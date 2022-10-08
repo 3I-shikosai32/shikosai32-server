49271e0a491a8e9cbdf3473980aab57f4f5ba015
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-08 15:23:59 +0900

    feat: ✨ (character-status/dto) ItemCompletedHistoryOrderInputを作成

---
+++ src/module/character-status/controller/dto/input/item-completed-history-order.input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { SortOrder } from '@/infra/prisma/generated/prisma/sort-order/enum';
+
+@InputType()
+export class ItemCompletedHistoryOrderInput {
+  @Field(() => SortOrder, { nullable: true })
+  createdAt?: keyof typeof SortOrder;
+
+  @Field(() => SortOrder, { nullable: true })
+  deliveredAt?: keyof typeof SortOrder;
+}
