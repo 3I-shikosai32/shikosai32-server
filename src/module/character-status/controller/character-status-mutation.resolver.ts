ea6b94290f9407e0c290f5f5f893edb4b8fb2292
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-10 20:30:25 +0900

    fix: 🐞 (CharacterStatusMutation) changeDeliveryStatusCharacterStatusを改名

--- src/module/character-status/controller/character-status-mutation.resolver.ts
+++ src/module/character-status/controller/character-status-mutation.resolver.ts
@@ -2,7 +2,7 @@ import { Inject, Logger } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CharacterStatus as CharacterStatusModel } from '../domain/model/character-status.model';
 import { CharacterStatusDeliveryManagerUseCaseInterface } from '../domain/service/use-case/character-status-delivery-manager.use-case';
-import { ChangeDeliveryStatusCharacterStatusArgs } from './dto/args/change-delivery-status-character-status.args';
+import { ChangeDeliveryStateCharacterStatusArgs } from './dto/args/change-delivery-state-character-status.args';
 import { CharacterStatus } from './dto/object/character-status.object';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
 
@@ -16,11 +16,11 @@ export class CharacterStatusMutation {
   ) {}
 
   @Mutation(() => CharacterStatus)
-  async changeDeliveryStatusCharacterStatus(@Args() args: ChangeDeliveryStatusCharacterStatusArgs): Promise<CharacterStatusModel> {
-    this.logger.log('changeDeliveryStatusCharacterStatus called');
+  async changeDeliveryStateCharacterStatus(@Args() args: ChangeDeliveryStateCharacterStatusArgs): Promise<CharacterStatusModel> {
+    this.logger.log('changeDeliveryStateCharacterStatus called');
     this.logger.log(args);
 
-    const updatedCharacterStatus = await this.characterStatusDeliveryManagerUseCase.changeDeliveryStatus(args.where.id, args.delivered);
+    const updatedCharacterStatus = await this.characterStatusDeliveryManagerUseCase.changeDeliveryState(args.where.id, args.delivered);
 
     return updatedCharacterStatus;
   }
