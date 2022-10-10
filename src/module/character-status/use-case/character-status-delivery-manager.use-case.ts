139e449688c81af4b9730f1d354798a0405060a5
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-10 19:05:05 +0900

    feat: ✨ (character-status/use-case) CharacterStatusDeliveryManagerUseCaseを作成

---
+++ src/module/character-status/use-case/character-status-delivery-manager.use-case.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CharacterStatusRepositoryInterface } from '../domain/service/repository/character-status.repository';
+import { CharacterStatusDeliveryManagerUseCaseInterface } from '../domain/service/use-case/character-status-delivery-manager.use-case';
+import { InjectionToken } from '@/common/constant/injection-token.constant';
+
+@Injectable()
+export class CharacterStatusDeliveryManagerUseCase implements CharacterStatusDeliveryManagerUseCaseInterface {
+  constructor(
+    @Inject(InjectionToken.CHARACTER_STATUS_REPOSITORY)
+    private readonly characterStatusRepository: CharacterStatusRepositoryInterface,
+  ) {}
+
+  async changeDeliveryStatus(characterStatusId: string, isDelivered: boolean) {
+    const updatedCharacterStatus = await this.characterStatusRepository.update({
+      where: { id: characterStatusId },
+      data: {
+        itemCompletedHistory: {
+          isDelivered,
+          deliveredAt: isDelivered ? new Date() : null,
+        },
+      },
+    });
+
+    return updatedCharacterStatus;
+  }
+}
