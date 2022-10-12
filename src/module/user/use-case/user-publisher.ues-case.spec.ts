3930233a51fc33428a74b1179dc25edb5292fcee
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-12 10:24:51 +0900

    feat: ✨ (UserPublisherUseCase) publishRankingを追加

--- src/module/user/use-case/user-publisher.ues-case.spec.ts
+++ src/module/user/use-case/user-publisher.ues-case.spec.ts
@@ -1,5 +1,7 @@
 import { Test } from '@nestjs/testing';
+import { RankingTarget } from '../controller/dto/enum/ranking-target.enum';
 import { GameAttenders } from '../domain/model/game-attenders.model';
+import { User } from '../domain/model/user.model';
 import { MockedUserRepository } from '../repository/mocked-user.repository';
 import { UserPublisherUseCase } from './user-publisher.ues-case';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
@@ -14,6 +16,12 @@ describe('UserPublisherUseCase', () => {
     }).compile();
 
     userPublisherUseCase = moduleRef.get(UserPublisherUseCase);
+  });
+
+  test('publishRanking', async () => {
+    const rankedUsers = await userPublisherUseCase.publishRanking(RankingTarget.CAT, true);
+
+    expect(rankedUsers).toEqual(expect.any(Array<User>));
   });
 
   test('publishUpdatedGameAttenders', async () => {
