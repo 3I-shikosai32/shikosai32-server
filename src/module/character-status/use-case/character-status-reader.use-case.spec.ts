9d94e01dae2d18962208a531f5170cab981f2363
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-12 09:59:15 +0900

    feat: ✨ (CharacterStatusReaderUseCase) findIncludeCharacterFromUserIdsを追加

--- src/module/character-status/use-case/character-status-reader.use-case.spec.ts
+++ src/module/character-status/use-case/character-status-reader.use-case.spec.ts
@@ -1,19 +1,35 @@
 import { Test } from '@nestjs/testing';
+import { Character } from '@prisma/client';
 import { MockedCharacterStatusRepository } from '../repository/mocked-character-status.repository';
 import { CharacterStatusReaderUseCase } from './character-status-reader.use-case';
 import { InjectionToken } from '@/common/constant/injection-token.constant';
+import { MockedUserRepository } from '~/user/repository/mocked-user.repository';
 
 describe('CharacterStatusReaderUseCase', () => {
+  let mockedUserRepository: MockedUserRepository;
   let mockedCharacterStatusRepository: MockedCharacterStatusRepository;
   let characterStatusReaderUseCase: CharacterStatusReaderUseCase;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [{ provide: InjectionToken.CHARACTER_STATUS_REPOSITORY, useClass: MockedCharacterStatusRepository }, CharacterStatusReaderUseCase],
+      providers: [
+        { provide: InjectionToken.USER_REPOSITORY, useClass: MockedUserRepository },
+        { provide: InjectionToken.CHARACTER_STATUS_REPOSITORY, useClass: MockedCharacterStatusRepository },
+        CharacterStatusReaderUseCase,
+      ],
     }).compile();
 
+    mockedUserRepository = moduleRef.get(InjectionToken.USER_REPOSITORY);
     mockedCharacterStatusRepository = moduleRef.get(InjectionToken.CHARACTER_STATUS_REPOSITORY);
     characterStatusReaderUseCase = moduleRef.get(CharacterStatusReaderUseCase);
+  });
+
+  test('findIncludeCharacterFromUserIds', async () => {
+    const expectUsers = await mockedUserRepository.findMany();
+
+    const foundCharacters = await characterStatusReaderUseCase.findIncludeCharacterFromUserIds(expectUsers.map((user) => user.id));
+
+    expect(foundCharacters).toEqual(expect.any(Array<Character>));
   });
 
   test('findItemCompletedCharacterStatuses', async () => {
