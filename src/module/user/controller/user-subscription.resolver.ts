2ec9e009452a1c9c68166fbab28446c37851f388
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-12 10:26:55 +0900

    feat: ✨ (UserSubscription) updatedRanking GraphQL Subscriptionを作成

--- src/module/user/controller/user-subscription.resolver.ts
+++ src/module/user/controller/user-subscription.resolver.ts
@@ -1,11 +1,18 @@
-import { Resolver, Subscription } from '@nestjs/graphql';
+import { Args, Resolver, Subscription } from '@nestjs/graphql';
+import { UpdatedRankingArgs } from './dto/args/updated-ranking.args';
 import { GameAttenders } from './dto/object/game-attenders.object';
-import { PubSubTrigger } from '@/common/constant/pubsub-iterator.constant';
+import { User } from './dto/object/user.object';
+import { generateUpdatedRankingTrigger, PubSubTrigger } from '@/common/constant/pubsub-iterator.constant';
 import { PubSubService } from '@/infra/pubsub/pubsub.service';
 
 @Resolver()
 export class UserSubscription {
   constructor(private readonly pubSubService: PubSubService) {}
+
+  @Subscription(() => [User])
+  async updatedRanking(@Args() args: UpdatedRankingArgs) {
+    return this.pubSubService.asyncIterator(generateUpdatedRankingTrigger(args.rankingTarget));
+  }
 
   @Subscription(() => GameAttenders)
   async updatedGameAttenders() {
