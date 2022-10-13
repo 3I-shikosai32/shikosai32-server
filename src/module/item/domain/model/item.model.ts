181b2dce4a2f915429d5c1a9c61121d48199d954
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-14 06:18:06 +0900

    feat: ✨ (item/domain-model) iconUrlフィールド及びlayerUrlを追加

--- src/module/item/domain/model/item.model.ts
+++ src/module/item/domain/model/item.model.ts
@@ -3,7 +3,9 @@ import { Character } from '@/infra/prisma/generated/prisma/character/enum';
 export class Item {
   readonly id: string;
 
-  readonly url: string;
+  readonly iconUrl: string;
+
+  readonly layerUrl: string;
 
   readonly character: keyof typeof Character;
 
@@ -11,9 +13,17 @@ export class Item {
 
   readonly characterStatusIds: string[];
 
-  constructor(args: { id: string; url: string; character: keyof typeof Character; layer: number; characterStatusIds: string[] }) {
+  constructor(args: {
+    id: string;
+    iconUrl: string;
+    layerUrl: string;
+    character: keyof typeof Character;
+    layer: number;
+    characterStatusIds: string[];
+  }) {
     this.id = args.id;
-    this.url = args.url;
+    this.iconUrl = args.iconUrl;
+    this.layerUrl = args.layerUrl;
     this.character = args.character;
     this.layer = args.layer;
     this.characterStatusIds = args.characterStatusIds;
