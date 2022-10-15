0d4fdcc080e579ccb84fbc8e9e2041a077b6c75f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-15 12:14:29 +0900

    feat: ✨ (prisma/seed) ItemSeederを作成

---
+++ src/infra/prisma/seed/seeder/item.seeder.ts
@@ -0,0 +1,39 @@
+import { Character, Prisma, PrismaClient } from '@prisma/client';
+
+export class ItemSeeder {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findItems() {
+    const items = await this.prisma.item.findMany();
+
+    return items;
+  }
+
+  async createItems() {
+    await this.prisma.item.createMany({
+      data: Array.from({ length: 4 }).map<Prisma.ItemCreateManyInput>((_, index) => ({
+        iconUrl: `https://firebasestorage.googleapis.com/v0/b/i-shikosai32.appspot.com/o/sys%2Fcharacter%2Fcat%2Fitem%2${index}.icon.svg?alt=media`,
+        layerUrl: `https://firebasestorage.googleapis.com/v0/b/i-shikosai32.appspot.com/o/sys%2Fcharacter%2Fcat%2Fitem%2${index}.layer.svg?alt=media`,
+        character: Character.CAT,
+        layer: index,
+      })),
+    });
+  }
+
+  async updateItems(itemIds: string[], characterStatusIds: string[]) {
+    await this.prisma.item.updateMany({
+      where: {
+        id: {
+          in: itemIds,
+        },
+      },
+      data: {
+        characterStatusIds,
+      },
+    });
+  }
+
+  async deleteItems() {
+    await this.prisma.item.deleteMany();
+  }
+}
