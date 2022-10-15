155cc80997e4c4c7c80e295424f89a10a2e2473d
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-15 12:15:08 +0900

    feat: ✨ (prisma/seed) UserSeederを作成

---
+++ src/infra/prisma/seed/seeder/user.seeder.ts
@@ -0,0 +1,24 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+export class UserSeeder {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findUsers() {
+    const users = await this.prisma.user.findMany();
+
+    return users;
+  }
+
+  async createUsers() {
+    await this.prisma.user.createMany({
+      data: Array.from({ length: 30 }).map<Prisma.UserCreateManyInput>((_, index) => ({
+        name: `user-${index < 10 ? `0${index}` : index}`,
+        email: `user-${index < 10 ? `0${index}` : index}@example.com`,
+      })),
+    });
+  }
+
+  async deleteUsers() {
+    await this.prisma.user.deleteMany();
+  }
+}
