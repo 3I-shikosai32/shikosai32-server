dbe76c5011e91d308bb64f50de86905129edfaac
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-16 22:13:05 +0900

    fix: 🐞 (user/dto) UpdatedRankingArgsにdateフィールドを追加

--- src/module/user/controller/dto/args/updated-ranking.args.ts
+++ src/module/user/controller/dto/args/updated-ranking.args.ts
@@ -1,8 +1,12 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { Date } from '../enum/date.enum';
 import { RankingTarget } from '../enum/ranking-target.enum';
 
 @ArgsType()
 export class UpdatedRankingArgs {
   @Field(() => RankingTarget, { nullable: false })
   rankingTarget: keyof typeof RankingTarget;
+
+  @Field(() => Date, { nullable: false })
+  date: keyof typeof Date;
 }
