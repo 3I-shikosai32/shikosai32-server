a6a37988f571077e55a23222a9bdf75f5e3c9fbe
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-19 14:14:42 +0900

    fix: 🐞 (UserReaderUseCase) getRankingが返す型をRankingに修正

--- src/module/user/domain/service/use-case/user-reader.use-case.ts
+++ src/module/user/domain/service/use-case/user-reader.use-case.ts
@@ -1,5 +1,6 @@
 import { GameAttenders } from '../../model/game-attenders.model';
 import { ObtainmentStatus } from '../../model/obtainment-status.model';
+import { Ranking } from '../../model/ranking.model';
 import { User } from '../../model/user.model';
 import { UserCursor, UserOrderBy, UserWhere } from './port/user-reader.input';
 import { Date } from '~/user/controller/dto/enum/date.enum';
@@ -10,6 +11,6 @@ export interface UserReaderUseCaseInterface {
   findUsers(where?: UserWhere, orderBy?: UserOrderBy[], cursor?: UserCursor, take?: number, skip?: number): Promise<User[]>;
   getObtainmentStatuses(userId: string): Promise<ObtainmentStatus[]>;
   getRankingPosition(userId: string, date: Date): Promise<number>;
-  getRanking(rankingTarget: keyof typeof RankingTarget, date: keyof typeof Date, take?: number): Promise<User[]>;
+  getRanking(rankingTarget: keyof typeof RankingTarget, date: keyof typeof Date, take?: number): Promise<Ranking[]>;
   getGameAttenders(): Promise<GameAttenders>;
 }
